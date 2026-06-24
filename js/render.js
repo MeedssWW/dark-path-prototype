bc107673f33ff6d4da4e79e6e4134ed58aa2da4c
pasha <samk1gha@gmail.com>
2026-06-24 14:29:11 +0500

    fix: regenerate sword and dagger hand images to improve weapon alignment and realism

--- js/render.js
+++ js/render.js
@@ -354,7 +354,7 @@ function renderHands() {
     els.realLeftArm.style.display = 'block';
   }
   // Weapon and Offhand
-  const v = 21;
+  const v = 22;
   
   if (els.realRightArm) {
     els.realRightArm.style.display = 'block';
